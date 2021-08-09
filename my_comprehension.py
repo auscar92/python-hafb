3317d0e59f0a5af9b45018a32802c75e2e7f3a8a
t25 <auscar92@gmail.com>
2021-08-09 14:08:40 -0600

    Added my_comprehension.py

---
+++ my_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Author : t25 <me@wsu.com>
+Date   : 8/9/2021
+Purpose:
+"""
+
+
+# --------------------------------------------------
+def main():
+    """Iterables and Comprehensions"""
+    words = "Today I am very happy because Barcelona does not have Messi with them any more"
+    print(words)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()

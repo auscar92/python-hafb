b5757a3433985d7ea10a057633a2ff0dde52bd03
t25 <auscar92@gmail.com>
2021-08-09 09:34:07 -0600

    Moved functions into a module

--- hello.py
+++ hello.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
-# Purpose: Say Hello to Waldo
+"""
+Author: Austin Carey
+Purpose: Say Hello to Waldo
+"""
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('name', help='Name to greet')
-args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+
+def get_args():
+    """
+    Get the command-line arguments
+    :return: list of parsed arguments
+    """
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
 
 
-# print("Hello Waldo")
+def main():
+    """
+    This is where the action is
+    :return: Void
+    """
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
+if __name__ == '__main__':
+    main()

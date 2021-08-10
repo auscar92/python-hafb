72b09fd6025ae0216f78568ab74cad4c137092c4
t25 <auscar92@gmail.com>
2021-08-10 09:22:41 -0600

    Updated my_itertools.py

--- Day2/my_itertools.py
+++ Day2/my_itertools.py
@@ -2,69 +2,37 @@
 """
 Author : t25 <me@wsu.com>
 Date   : 8/10/2021
-Purpose:
+Purpose: Review itertools package
 """
-
-import argparse
-
-
-# --------------------------------------------------
-def get_args():
-    """Get command-line arguments"""
-
-    parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
-
-    return parser.parse_args()
+from itertools import islice, count
+from Day1 import my_comprehension as mc
 
 
 # --------------------------------------------------
 def main():
     """Make your noise here"""
+    # Task: Create a list of the first 1000 prime numbers
+    #thousand_primes = islice((x for x in count() if mc.is_prime(x)), 1000)
+    #print(list(thousand_primes))
 
-    args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    # Use the built-ins any, all
+    print(any([False, False, True]))
+    print(all([False, False, True]))
+    cities = ['London', 'Madrid', 'New York', 'Ogden']
+    # print(f'The cities list is {all(city == city.title() for city in cities)} to go')
+    print(f'The cities list is {all([city[0].isupper() for city in cities])} to go')
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    # Use built-in zip
+    sunday = [12, 14, 15, 15, 17, 21, 22, 22, 23, 22, 20, 18]
+    monday = [13, 14, 14, 16, 20, 21, 22, 22, 21, 19, 18, 16]
+    tuesday = [12, 13, 14, 16, 20, 20, 21, 20, 19, 16, 14, 12]
+
+    # for item in zip(sunday, monday):
+    for sun, mon in zip(sunday, monday):
+        print(f'The average = {(sun+mon)/2.0}')
+    for temp in zip(sunday, monday, tuesday):
+        #print(f'minimum = {min(temp)}, maximum = {max(temp)}, and avg = {(sum(temp)/len(temp))}')
+        print(f'minimum = {min(temp):4.1f}, maximum = {max(temp):4.1f}, and avg = {(sum(temp)/len(temp)):4.1f}')
 
 
 # --------------------------------------------------

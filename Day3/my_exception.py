7dd33a0535b0ee8c4793210b5b467f69929a9c0f
t25 <auscar92@gmail.com>
2021-08-11 10:18:54 -0600

    added median tests

--- Day3/my_exception.py
+++ Day3/my_exception.py
@@ -7,13 +7,65 @@ Purpose:
 from random import randrange
 
 
+def median(iterable):
+    """
+    Obtain the center value of a series
+    Sort the iterable and return the middle value if there is an odd
+    number of elements, or the arithmetic mean of the middle two elements
+    if there is an even number of elements
+    :param iterable: a series of iterable items
+    :exception ValueError for empty sequence
+    :return: the median value
+    """
+    if len(iterable) == 0:
+        raise ValueError('median() arg is an empty sequence')
+
+    iterable.sort()
+    half = (len(iterable)-1) // 2
+    if len(iterable) % 2 == 0:
+        return (iterable[half] + iterable[half+1]) / 2.0
+    else:
+        return iterable[half]
+
+
+def test_median():
+    # print(f'Median odd= {median([5, 2, 1, 4, 3])}')
+    # print(f'Median even= {median([5, 2, 1, 4, 3, 6])}')
+    # print(f'Median even= {median([])}')
+    try:
+        median([])
+    except ValueError as e:
+        print(f'Payload: {e.args}')
+        print(f'{str(e)}')
+
+
+
+def lookups():
+    s = [1, 4, 6]
+    try:
+        item = s[5]
+    #except IndexError: # Is a subclass of LookupError
+    except LookupError: # first
+        print('Handled IndexError')
+
+    d = dict(a=65, b=33)
+    try:
+        value = d['x']
+    # except KeyError: # is a subclass of LookupError
+    except LookupError: # first
+        print('Handled KeyError')
+
+
 # --------------------------------------------------
 def main():
     """Make your noise here"""
     # Random number between 0-99
-    number = randrange(100)
+    number = randrange(10)
     while True:
-        guess = int(input('Guess a number:'))
+        try:
+            guess = int(input('Guess a number:'))
+        except ValueError:
+            continue
         if guess == number:
             print('You got it')
             break
@@ -21,4 +73,6 @@ def main():
 
 # --------------------------------------------------
 if __name__ == '__main__':
-    main()
+    #main()
+    #lookups()
+    test_median()

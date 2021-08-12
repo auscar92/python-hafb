cf9c474782599307076293af5aa5bfe04767ded3
t25 <auscar92@gmail.com>
2021-08-12 11:17:14 -0600

    Practiced with class decorators in class_decorator.py

--- Day4/class_decorator.py
+++ Day4/class_decorator.py
@@ -2,69 +2,34 @@
 """
 Author : t25 <me@wsu.com>
 Date   : 8/12/2021
-Purpose:
+Purpose: Class as decorators
 """
 
-import argparse
+
+class DecoratedClass(object):
+    def __init__(self, original_function):
+        self._original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print(f'Call method executed this before {self._original_function.__name__}')
+        return self._original_function(*args, **kwargs)
 
 
-# --------------------------------------------------
-def get_args():
-    """Get command-line arguments"""
+@DecoratedClass
+def display():
+    print('Display ran')
 
-    parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('r'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
-
-    return parser.parse_args()
+@DecoratedClass
+def display_info(name, age):
+    print(f'Display function ran for {name} {age}')
 
 
 # --------------------------------------------------
 def main():
     """Make your noise here"""
-
-    args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    display()
+    display_info('Waldo', 21)
 
 
 # --------------------------------------------------

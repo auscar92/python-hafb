bc554310d5eefd4fc208708d2cae268b19d70d1d
t25 <auscar92@gmail.com>
2021-08-12 14:24:03 -0600

    Calculated gdp growth

--- Day4/my_matplot.py
+++ Day4/my_matplot.py
@@ -57,6 +57,55 @@ def continent_data(data):
     # Print number of countries per continent
     print(f'Asia countries: {len(set(asia_2007.country))}')
     print(f'Europe countries: {len(set(europe_2007.country))}')
+    # Get mean and median
+    print(f'Mean GDP in Asia {asia_2007.gdpPerCapita.mean()}')
+    print(f'Median GDP in Asia {asia_2007.gdpPerCapita.median()}')
+    print(f'Mean GDP in Europe {europe_2007.gdpPerCapita.mean()}')
+    print(f'Median GDP in Europe {europe_2007.gdpPerCapita.median()}')
+    # make subplots
+    plt.subplot(2, 1, 1)
+    plt.title('Distribution of GDP Per Capita')
+    plt.hist(asia_2007.gdpPerCapita, 20, range=(0, 50000), edgecolor='black')
+    plt.ylabel('Asia')
+    plt.subplot(2, 1, 2)
+    plt.hist(europe_2007.gdpPerCapita, 20, range=(0, 50000), edgecolor='black')
+    plt.ylabel('Europe')
+    plt.show()
+
+
+def continent_data_le(data):
+    """Compare the life expectancy distribution between
+    Europe and America in 1997"""
+    continents = set(data.continent)
+    data_1997 = data[data.year == 1997]
+    americas_1997 = data_1997[data_1997.continent == 'Americas']
+    europe_1997 = data_1997[data_1997.continent == 'Europe']
+    # make subplots
+    bins = 20
+    plt.subplot(211)
+    plt.title('Distribution of Life Expectancy')
+    plt.hist(americas_1997.lifeExpectancy, bins, range=(55, 85), edgecolor='black')
+    plt.ylabel('Americas')
+    plt.subplot(212)
+    plt.hist(europe_1997.lifeExpectancy, bins, range=(55, 88), edgecolor='black')
+    plt.ylabel('Europe')
+    plt.show()
+
+
+def continent_data_gdp_growth(data):
+    usa = data[data.country == 'United States']
+    china = data[data.country == 'China']
+    # Calculate growth
+    usa_growth = usa.gdpPerCapita / (usa.gdpPerCapita.iloc[0]*100)
+    china_growth = china.gdpPerCapita / (china.gdpPerCapita.iloc[0]*100)
+
+    plt.title('GDP per Capita Growth(first year = 100)')
+    plt.plot(usa.year, usa_growth)
+    plt.plot(china.year, china_growth)
+    plt.legend(['United States', 'China'])
+    plt.xlabel('Year')
+    plt.ylabel('GDP per capita growth')
+    plt.show()
 
 
 # --------------------------------------------------
@@ -64,7 +113,9 @@ def main():
     """Make your noise here"""
     data = pd.read_csv('countries.csv')
     #import_data_pandas(data)
-    continent_data(data)
+    #continent_data(data)
+    #continent_data_le(data)
+    continent_data_gdp_growth(data)
 
 
 # --------------------------------------------------
